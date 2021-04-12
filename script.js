f902ff668c8f14acf62bf7d527c3c759c833dea2
MSebb23 <matteosabbatini23@gmail.com>
2021-04-12 15:36:43 +0200

    first styling and basic html

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-alert("heloooo");
+var app_Vue = new Vue (
+    {
+        el : "#app_Vue",
+        data : {
+            movie : [],
+        },
+        mounted : function () {
+            axios.get("something").then((answer) => ) {
+
+            }
+        }
+    }
+)
+
+
+/*
+var app_Vue = new Vue (
+    {
+        el : "#app_Vue",
+        data : {
+            album_arr : [],
+            genre : [],
+            chosen_genre : "",
+        },
+        mounted : function () {
+            axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((answer) => {
+
+                var sorted_album = answer.data.response.sort(function (num1, num2) {
+                    return  num2.year - num1.year;
+                })
+                // console.log(answer.data.response);
+                this.album_arr = sorted_album;
+
+                this.album_arr.forEach((element, index) => {
+                    console.log(this.genre);
+                    if (!this.genre.includes(element.genre)) {
+                        this.genre.push(element.genre)
+                    }
+                });
+            });
+        }
+
+    }
+);
+*/
