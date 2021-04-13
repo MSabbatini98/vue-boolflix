e0440657c5cef35055d0a9823478408284ae800e
MSebb23 <matteosabbatini23@gmail.com>
2021-04-13 02:22:03 +0200

    Added languages' flags
    Added flags imgs, and now the flags are corresponding to the languages, fixed some Css

--- script.js
+++ script.js
@@ -6,10 +6,28 @@ var app_Vue = new Vue (
         el : "#app_Vue",
         data : {
             movies : [],
-            
+            flags : {
+                'it' : 'italy',
+                'es' : 'spain',
+                'pt' : 'portugal',
+                'pl' : 'poland',
+                'en' : 'uk',
+                'de' : 'germany',
+                'br' : 'brazil',
+                // 'it' : 'japan',
+                // 'it' : 'china',
+                // 'it' : 'france',
+                // 'it' : 'israel',
+                // 'it' : 'finland',
+                // 'it' : 'greece',
+                // 'it' : 'sweden',
+                // 'it' : 'netherlands',
+            },
+            flag : "",
             user_input : "e",
         },
         methods : {
+            
             startSearch() {
                 axios.get("https://api.themoviedb.org/3/search/movie", 
                     {
@@ -19,7 +37,9 @@ var app_Vue = new Vue (
                         }
                     // senza arrow function, serve self = this
                     }).then((answer) => {
+                        // console.log(answer.data.results[0].original_language); //risultato della lingua del primo elemento
                         this.movies = answer.data.results;
+                        
                     }
                 );
             }
@@ -29,6 +49,18 @@ var app_Vue = new Vue (
 
 
 /*
+*this.movies.forEach(element => {
+                            const lungh = Object.keys(this.flags).length;
+                            for(var i = 0; i != lungh; i++) {
+                                console.log( lungh);
+                                console.log( element.original_language);
+                                if (element.original_language in this.flags ) {
+                                    console.log( element.original_language);
+                                }
+                
+                            }
+                            console.log( element.original_language);
+                        });
 var app_Vue = new Vue (
     {
         el : "#app_Vue",
