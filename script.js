1c7d2f5fe39d60a2156686091748c745c3bce899
MSebb23 <matteosabbatini23@gmail.com>
2021-04-13 17:40:01 +0200

    Error Hotwell
    params in or out of data

--- script.js
+++ script.js
@@ -1,74 +1,57 @@
 //!  url base del query : https://api.themoviedb.org/3
-// ! GET url : /search/movie
+// ! GET url1 : /search/movie
+// ! GET url2 : /search/tv
 // ! API Key : api_key=44c0abd41cd7f15ce525a6fdbc83e665 
 var app_Vue = new Vue (
     {
         el : "#app_Vue",
         data : {
+            
+            show_error : true,
+            api_key : '44c0abd41cd7f15ce525a6fdbc83e665',
+            language : "it-IT",
+            
             movies : [],
             flag : "",
             user_input : "e",
+            params: {
+                api_key : '44c0abd41cd7f15ce525a6fdbc83e665',
+                query: this.user_input,
+                language : "it-IT",
+            }
         },
+
         methods : {
-            
+            // errore () {
+            //     console.log("eHELLO");
+            //     this.movies = [];
+
+            // },
             startSearch() {
-                axios.get("https://api.themoviedb.org/3/search/movie", 
-                    {
-                        params: {
-                            api_key : '44c0abd41cd7f15ce525a6fdbc83e665',
-                            query: this.user_input,
+                if (this.user_input){
+
+                    axios.get("https://api.themoviedb.org/3/search/movie",
+                        {
+                            params : this.params,
                         }
-                    // senza arrow function, serve self = this
-                    }), 
-                axios.get("https://api.themoviedb.org/3/search/tv",
-                    {
-                        params: {
-                            api_key : '44c0abd41cd7f15ce525a6fdbc83e665',
-                            query: this.user_input,
+                    ).then((answer) => {
+                        this.movies.push(...answer.data.results);       
                         }
-                    }).then((answer) => {
-                        // console.log(answer.data.results[0].original_language); //risultato della lingua del primo elemento
-                        this.movies = answer.data.results;
-                        
-                    }
-                ).catch((error) => {
-                    console.log("c'è un erroreeee")
-                }
-            )
-        }
-    }
-});
-/*startSearch() {
-                axios.get("https://api.themoviedb.org/3/search/movie", 
-                    {
-                        params: {
-                            api_key : '44c0abd41cd7f15ce525a6fdbc83e665',
-                            query: this.user_input,
+                    );
+                    axios.get("https://api.themoviedb.org/3/search/tv",
+                        {
+                            params : this.params,
+
                         }
-                    // senza arrow function, serve self = this
-                    }).then((answer) => {
-                        // console.log(answer.data.results[0].original_language); //risultato della lingua del primo elemento
-                        this.movies = answer.data.results;
-                        
-                    }).catch((error) => {
-                    console.log("c'è un erroreeee", error)
-                });
-                axios.get("https://api.themoviedb.org/3/search/tv",
-                    {
-                        params: {
-                            api_key : '44c0abd41cd7f15ce525a6fdbc83e665',
-                            query: this.user_input,
+                    ).then((answer) => {
+                        this.movies.push(...answer.data.results);       
                         }
-                    }).then((answer) => {
-                        // console.log(answer.data.results[0].original_language); //risultato della lingua del primo elemento
-                        this.tv = answer.data.results;
-                        
-                    }).catch((error) => {
-                    console.log("c'è un erroreeee", error)
-                }
-            );
-            this.output.push(this.movies, this.tv)
-        }
-    }
-});
-*/
+                    );
+                }//?chiudo l'if
+            } //?finisce la funzione startSearch
+        }, // ? finiscono i methods
+        mounted: function () {
+            console.log("23");
+        },
+    }   
+);
